f58de4d47a0d42f7d6714db4aa2e95ec103e65a7
Yan Wei <asurada.gen@gmail.com>
2013-08-01 16:47:08 +0900

    new
    初期追加

---
+++ sangokuCut/AppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.h
+//  sangokuCut
+//
+//  Created by 厳 巍 on 2013/08/01.
+//  Copyright 厳 巍 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+// Added only for iOS 6 support
+@interface MyNavigationController : UINavigationController <CCDirectorDelegate>
+@end
+
+@interface AppController : NSObject <UIApplicationDelegate>
+{
+	UIWindow *window_;
+	MyNavigationController *navController_;
+	
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) MyNavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ sangokuCut/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  sangokuCut
+//
+//  Created by 厳 巍 on 2013/08/01.
+//  Copyright 厳 巍 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
